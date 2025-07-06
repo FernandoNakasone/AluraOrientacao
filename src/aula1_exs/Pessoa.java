159390592d114ab28b2c273103a1ef1c9294cdc6
FernandoNakasone <FernandoNakasone040@gmail.com>
2025-07-05 23:50:35 -0300

    Exemplo Pessoa

---
+++ src/aula1_exs/Pessoa.java
@@ -0,0 +1,10 @@
+package aula1_exs;
+
+public class Pessoa {
+    String nome;
+    int idade;
+
+    void fazAniversario(){
+        idade++;
+    }
+}
--- src/aula_1/Principal.java
+++ src/aula_1/Principal.java
@@ -1,14 +1,23 @@
 package aula_1;
 
+import java.text.DecimalFormat;
+
 public class Principal {
     public static void main(String[] args) {
+        DecimalFormat dF = new DecimalFormat("#,##0.00");
         Filme filme1 = new Filme();
 
         filme1.nome = "O poderoso chefão";
         filme1.anoDeLancamento = 1972;
         filme1.duracaoEmMinutos = 180;
 
-        System.out.println(filme1.nome);
-        System.out.println(filme1.anoDeLancamento);
+        filme1.exibeFichaTecnica();
+        filme1.avalia(8);
+        filme1.avalia(5);
+        filme1.avalia(10);
+
+        System.out.println("Soma das avaliações:" + filme1.somaDasAvaliacao);
+        System.out.println("Total de avaliações:" + filme1.totalDeAvaliacoes);
+        System.out.println("Média:" + dF.format(filme1.pegaMedia()));
     }
 }
