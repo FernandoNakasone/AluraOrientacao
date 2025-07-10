3253f1c87ce8e6e6ff2f5642fe80235472d3ddd3
FernandoNakasone <FernandoNakasone040@gmail.com>
2025-07-10 00:31:41 -0300

    exercicios da aula 2 finalizados

---
+++ src/exercicios_exemplos/aula2/Livro.java
@@ -0,0 +1,26 @@
+package exercicios_exemplos.aula2;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void exibiDetalhes(){
+        System.out.println("Título:" + titulo + " - Autor:" + autor);
+    }
+}
